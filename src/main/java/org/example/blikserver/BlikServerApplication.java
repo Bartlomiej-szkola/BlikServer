d54383f547832538f931c67dd400088e6b629f20
Bartlomiej-szkola <Bartlomiej.Tobiasz@tm1.edu.pl>
2026-03-09 12:43:28 +0100

    init

---
+++ src/main/java/org/example/blikserver/BlikServerApplication.java
@@ -0,0 +1,13 @@
+package org.example.blikserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlikServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BlikServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/blikserver/BlikServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.blikserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlikServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
